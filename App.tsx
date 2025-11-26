90c021918576074225450e97bd7acabe9c6e076e
Talles <tallesjose86@gmail.com>
2025-11-26 20:35:10 -0300

    refactor: alteração da estrutura identificação tailwind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { ScreenContent } from '@/components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { LoginScreen } from '@/screens/login';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <LoginScreen />
       <StatusBar style="auto" />
     </>
   );
---
+++ screens/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function LoginScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-slate-500">
+            <Text className="text-6xl">Tela de Login</Text>
+        </View>
+    )
+}
