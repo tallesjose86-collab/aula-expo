a28529c1ddc4e6a6ffd0d2691de3df8b85665d03
Talles <tallesjose86@gmail.com>
2025-12-15 19:10:42 -0300

    add contador

--- src/screens/TelaContador.tsx
+++ src/screens/TelaContador.tsx
@@ -1,27 +1,25 @@
 import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
+import { ButtonCustomizado } from "@/components/botaoCustomizado";
 
 export function TelaContador() {
   const [contador, setContador] = useState(0);
 
   return (
     <View className="flex-1 items-center justify-center gap-5">
+      <Text className="text-3xl color-black">Contador</Text>
       <Text className="text-4xl">{contador}</Text>
 
-      <View className="flex-row gap-5">
-        <TouchableOpacity
-          className="bg-green-500 p-4 rounded-xl"
+      <View className="flex-row gap-5 p-24">
+        <ButtonCustomizado
+          title="+"
           onPress={() => setContador(contador + 1)}
-        >
-          <Text className="text-white text-xl"> + </Text>
-        </TouchableOpacity>
+        />
 
-        <TouchableOpacity
-          className="bg-red-500 p-4 rounded-xl"
+        <ButtonCustomizado
+          title="-"
           onPress={() => setContador(contador - 1)}
-        >
-          <Text className="text-white text-xl"> - </Text>
-        </TouchableOpacity>
+        />
       </View>
     </View>
   );
